32b06780208d015a1618ec503f19f128d13c7e8b
Vansh Madaan <madaanvansh68@gmail.com>
2025-03-23 10:39:29 +0530

    done1

--- Basic.java
+++ Basic.java
@@ -9,6 +9,10 @@ class Student{
 public class Basic {
 
     public static void main(String[] args) {
-
+Student s=new Student();
+        System.out.println(s.rno);
+//        Integer num=50;
+//        num=45;
+//        System.out.println(num);
     }
 }
